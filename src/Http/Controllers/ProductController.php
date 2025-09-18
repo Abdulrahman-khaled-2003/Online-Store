3f4ac3769a303488d8d7a86606fd8abd46a85027
Abderluhamn Khaled <abdokhaled23334@gmail.com>
2025-09-18 17:54:46 +0300

    Create a ProductController

---
+++ src/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Core\Exception\RecordNotFoundException;
+use App\Core\Session;
+use App\Http\Validation\FormValidation;
+
+require base_path("./Core/Exceptions/RecordNotFoundException.php");
+require base_path("./Core/Session.php");
+require base_path("Http/Validation/FormValidation.php");
+require "Controller.php";
+
+class ProductController extends Controller{
+
+}
