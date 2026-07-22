9a83d6fa4eac9b1dafe6f8a92f2d6ad7ab5cb9bc
Abdulrahman Khaled <abdokhaled1947@gmail.com>
2026-07-22 23:53:20 +0300

    create two abstract function up() and down() to handle database operations

--- src/Core/Migration.php
+++ src/Core/Migration.php
@@ -3,5 +3,10 @@
 namespace App\Core;
 
 Abstract class Migration{
-
+        public function __construct(public Database $db){
+            //
+        }
+        
+        abstract public function up() : void;
+        abstract public function down() : void;
 }
