9c53aad8356dcaa3bc358c16e63af8ede4e67b04
Abdulrahman Khaled <abdokhaled1947@gmail.com>
2026-07-23 00:29:19 +0300

    Create: createMigrationsTableIfNotExists()

--- src/Core/MigrationRunner.php
+++ src/Core/MigrationRunner.php
@@ -5,5 +5,16 @@ namespace App\Core;
 class MigrationRunner{
     public function __construct(protected $migrationsPath){
         //
+        $this->createMigrationsTableIfNotExists();
+        }
+
+    protected function createMigrationsTableIfNotExists() : void{
+        db()->execute("
+            CREATE TABLE IF NOT EXISTS migrations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                migration_name VARCHAR(255) NOT NULL,
+                batch INT NOT NULL
+            )
+        ");
     }
 }
