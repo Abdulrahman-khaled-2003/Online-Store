39cfdc2bb69cbd937c2b3b320fd6b7f5c0d95733
Abdulrahman Khaled <abdokhaled1947@gmail.com>
2026-07-23 11:35:32 +0300

    create resolveClassName() to convert fileName to className

--- src/Core/MigrationRunner.php
+++ src/Core/MigrationRunner.php
@@ -2,13 +2,16 @@
 
 namespace App\Core;
 
-class MigrationRunner{
-    public function __construct(protected $migrationsPath){
+class MigrationRunner
+{
+    public function __construct(protected $migrationsPath)
+    {
         //
         $this->createMigrationsTableIfNotExists();
-        }
+    }
 
-    protected function createMigrationsTableIfNotExists() : void{
+    protected function createMigrationsTableIfNotExists(): void
+    {
         db()->execute("
             CREATE TABLE IF NOT EXISTS migrations (
                 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,14 +22,23 @@ class MigrationRunner{
     }
 
     protected function getRanMigrations(): array
-{
-    $rows = db()->fetchAll("SELECT migration_name FROM migrations");
-    return array_column($rows, 'migration_name');
-}
+    {
+        $rows = db()->fetchAll("SELECT migration_name FROM migrations");
+        return array_column($rows, 'migration_name');
+    }
 
-protected function getNextBatchNumber(): int
-{
-    $result = db()->fetch("SELECT MAX(batch) as max_batch FROM migrations");
-    return ($result['max_batch'] ?? 0) + 1;
-}
+    protected function getNextBatchNumber(): int
+    {
+        $result = db()->fetch("SELECT MAX(batch) as max_batch FROM migrations");
+        return ($result['max_batch'] ?? 0) + 1;
+    }
+
+    protected function resolveClassName(string $migrationName): string
+    {
+        $withoutTimestamp = preg_replace('/^\d{4}_\d{2}_\d{2}_\d{6}_/', '', $migrationName);
+        $words = explode('_', $withoutTimestamp);
+        $words = array_map('ucfirst', $words);
+
+        return implode('', $words);
+    }
 }
